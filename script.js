c36da3c5d94245c6829fd25093ade799618fc30d
Pei <nocsillty@gmail.com>
2023-10-06 21:10:35 -0400

    added scripts, they don't work

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-var ans, val, guesses;  
- 
+var ans = 0, val = 0, guesses = 0;  
+
+// Start or Reset the game
+const playButton = document.getElementById("playButton");
+
+const guessText = document.getElementById("guess");
+const guessesText = document.getElementById("guesses");
+
+const resetButton = document.getElementById("resetButton")
+
 function reset() { 
     ans = (int)(Math.random * 100 + 1);
     val = 50; 
     guesses = 5; 
+
+    display(); 
 }
 
-function changeVal(i) { 
+// playButton.addEventListener("click", reset); 
+// resetButton.addEventListener("click", reset);
+
+//Functions affect the guess value. 
+const buttons = document.querySelectorAll("numedit");
+
+buttons.forEach(button => { 
+    button.addEventListener('click', changeVal)
+});
+
+
+function changeVal(event) { 
+    const i = event.target.dataset.param; 
     val += i; 
+    displayGuess();
+}
+
+function display() { 
+    displayGuess();
+    displayGuesses(); 
+}
+
+function displayGuess() { 
+    guessText.innerHTML = "Guess: " + val; 
+}
+
+function displayGuesses() { 
+    
 }
 
 function commit() { 
     if(val == ans) return true; 
     return false; 
 }
+
