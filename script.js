a76519c82e8a61db24b2c3dc66606184a160eaac
Pei <nocsillty@gmail.com>
2023-10-06 22:50:53 -0400

    fixed scripts

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-var ans = 0, val = 0, guesses = 0;  
+var ans = 0;
+var val = -1 
+var guesses = 0;  
 
 // Start or Reset the game
-const playButton = document.getElementById("playButton");
+playButton = document.getElementById("playButton");
 
-const guessText = document.getElementById("guess");
-const guessesText = document.getElementById("guesses");
+guessText = document.getElementById("guess");
+guessesText = document.getElementById("guesses");
 
-const resetButton = document.getElementById("resetButton")
-
-function reset() { 
-    ans = (int)(Math.random * 100 + 1);
-    val = 50; 
-    guesses = 5; 
-
-    display(); 
-}
-
-// playButton.addEventListener("click", reset); 
-// resetButton.addEventListener("click", reset);
-
-//Functions affect the guess value. 
-const buttons = document.querySelectorAll("numedit");
-
-buttons.forEach(button => { 
-    button.addEventListener('click', changeVal)
-});
-
-
-function changeVal(event) { 
-    const i = event.target.dataset.param; 
-    val += i; 
-    displayGuess();
-}
+resetButton = document.getElementById("resetButton")
 
 function display() { 
     displayGuess();
@@ -43,10 +20,41 @@ function displayGuess() {
 }
 
 function displayGuesses() { 
-    
+    guessesText.innerHTML = "Guesses: " + guesses;
 }
 
+function reset() { 
+    ans = parseInt(Math.random() * 100 + 1);
+    val = 50; 
+    guesses = 5; 
+
+    display(); 
+}
+
+playButton.addEventListener("click", reset); 
+resetButton.addEventListener("click", reset);
+
+//Functions affect the guess value. 
+const buttons = document.querySelectorAll(".numedit");
+
+buttons.forEach(button => { 
+    button.addEventListener('click', changeVal)
+});
+
+
+function changeVal(event) { 
+    const i = parseInt(event.target.dataset.param); 
+    if(val >= 0) {
+        if(val + i >= 0 && val + i <= 100) val += i;
+        else if(val + i < 0) val = 0;
+        else val = 100;
+    }
+    displayGuess();
+}
+
+
 function commit() { 
+    guesses--;
     if(val == ans) return true; 
     return false; 
 }
